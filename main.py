Added check of correct knee rotation and new scale of the body according to unity character

main.py
@@ -19,20 +19,22 @@ def main(argv):
     if argv[2] == 'photo':
         input_image = sys.argv[1]
         image = cv2.imread(input_image)
-        boxes, coord,shape = solver(image)
-        init_marker = initial_check(boxes, coord,shape)
-        assert init_marker == True, 'Initial check found error'
+        # boxes, coord,shape = solver(image)
+        # init_marker = initial_check(boxes, coord,shape)
+        # assert init_marker == True, 'Initial check found error'
 
-        сoords3d,coords2d = coords_create(image)
+        coords3d,coords2d = coords_create(image)
         second_marker = check_sides(coords2d,50)
 
         assert second_marker == True, 'Second check found Error'
 
 
-        сoords3d = normalize_body(сoords3d,unity_body_length)
-        # angle = human_angles(сoords3d)
+        coords3d = knee_check(coords3d)
+        coords3d = normalize_body(coords3d,unity_body_length)
+        
+        # angle = human_angles(coords3d)
 
-        coords_arr = np.array(list(сoords3d.values()))
+        coords_arr = np.array(list(coords3d.values()))
         coords_list = [round(num, 4) for num in coords_arr.reshape(-1)]
         msg = "".join(str(x) + " " for x in coords_list) # create massege 
 
@@ -52,9 +54,9 @@ def main(argv):
         vidcap = cv2.VideoCapture(input_video)
         success,image = vidcap.read()
 
-        boxes, coord,shape = solver(image)
-        init_marker = initial_check(boxes, coord,shape)
-        assert init_marker == True, 'Initial check found error'
+        # boxes, coord,shape = solver(image)
+        # init_marker = initial_check(boxes, coord,shape)
+        # assert init_marker == True, 'Initial check found error'
 
         coordinates3D =[]
 
@@ -62,29 +64,32 @@ def main(argv):
         while success:
             
 
-            сoords3d,coords2d = coords_create(image)
+            coords3d,coords2d = coords_create(image)
 
             if count % 10 == 0:
                 second_marker = check_sides(coords2d,50)
-
                 assert second_marker == True, 'Second check found Error'
 
-            coordinates3D.append(сoords3d) # не знаю что лучше сначала в список потом передача, или предавать поочередно, пусть будет поочередно
+            # show video
+            cv2.imshow('Frame',image)
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+            # coordinates3D.append(сoords3d) # не знаю что лучше сначала в список потом передача, или предавать поочередно, пусть будет поочередно
             success,image = vidcap.read()
             count += 1
         
-    
+            coords3d = knee_check(coords3d)
+            coords3d = normalize_body(coords3d,unity_body_length)
+            # angle = human_angles(сoords3d)
 
-            сoords3d = normalize_body(сoords3d,unity_body_length)
-            angle = human_angles(сoords3d)
+            coords_arr = np.array(list(coords3d.values()))
+            coords_list = [round(num, 4) for num in coords_arr.reshape(-1)]
+            msg = "".join(str(x) + " " for x in coords_list) # create massege 
+            s.send(bytes(msg, "utf-8"))
 
-            while True:
-            # send massage to a server Антон тут нужно дописать что ты будешь передавать
-                msg = '%.4f %.4f %.4f %.4f'%(armX_1,armY_1,armX_2,armY_2)
-                s.send(bytes(msg, "utf-8"))
-                
-
-            s.close()
+        s.close()
 
 if __name__ == '__main__':
     main(sys.argv)

math_transformation.py
@@ -4,7 +4,11 @@ from params import *
 
 
 
-unity_body_length = (hip + spine + spine1 + spine2 + neck)[1]
+unity_body_length = np.linalg.norm(hip + spine + spine1 + spine2 + neck)
+unity_up_leg_length = np.linalg.norm(left_knee)
+unity_lower_leg_length = np.linalg.norm(left_ankle)
+unity_arm_length = np.linalg.norm(left_elbow)
+unity_forearm_length = np.linalg.norm(left_wrist)
 
 
 
@@ -123,26 +127,96 @@ def normalize_body(body_dict:dict, unity_body_length:float)->dict:
     Returns normalized body skeleton. Normalization is in accordance to Unity model
     """
     
-    scale_factor = unity_body_length / np.linalg.norm((body_dict['ass'] - body_dict['neck'])) # change -1 to 1 and reverse initial point of other body vectors
-    
-    new_body_dict = {'ass': np.array([0,0,0], dtype=np.float32)}
-    new_body_dict['neck'] = (body_dict['ass'] - body_dict['neck']) * scale_factor * np.array([-1,1,-1])
-    new_body_dict['chest'] = (body_dict['ass'] - body_dict['chest']) * scale_factor * np.array([-1,1,-1])
-    
-    new_body_dict['right_hip'] = (body_dict['ass'] - body_dict['right_hip']) * scale_factor * np.array([-1,1,-1])
-    new_body_dict['right_knee'] = (body_dict['right_hip'] - body_dict['right_knee']) * scale_factor * np.array([-1,1,-1])
-    new_body_dict['right_ankle'] = (body_dict['right_knee'] - body_dict['right_ankle']) * scale_factor * np.array([-1,1,-1])
+    image_body_length = np.linalg.norm((body_dict['ass'] - body_dict['neck']))
+    scale_factor_body = unity_body_length / image_body_length
 
-    new_body_dict['left_hip'] = (body_dict['ass'] - body_dict['left_hip']) * scale_factor * np.array([-1,1,-1])
-    new_body_dict['left_knee'] = (body_dict['left_hip'] - body_dict['left_knee']) * scale_factor * np.array([-1,1,-1])
-    new_body_dict['left_ankle'] = (body_dict['left_knee'] - body_dict['left_ankle']) * scale_factor * np.array([-1,1,-1])
+    # new_body_dict = {'ass': np.array([0,0,0], dtype=np.float32)}
+    new_body_dict = {'ass': get_ass_location(image_body_length, unity_body_length, unity_ass_position)}
+    new_body_dict['neck'] = (body_dict['ass'] - body_dict['neck']) * scale_factor_body * np.array([-1,1,-1])
+    new_body_dict['chest'] = (body_dict['ass'] - body_dict['chest']) * scale_factor_body * np.array([-1,1,-1])
     
-    new_body_dict['right_shoulder'] = (body_dict['chest'] - body_dict['right_shoulder']) * scale_factor * np.array([-1,1,-1])
-    new_body_dict['right_elbow'] = (body_dict['right_shoulder'] - body_dict['right_elbow']) * scale_factor * np.array([-1,1,-1])
-    new_body_dict['right_wrist'] = (body_dict['right_elbow'] - body_dict['right_wrist']) * scale_factor * np.array([-1,1,-1])
+    new_body_dict['right_hip'] = (body_dict['ass'] - body_dict['right_hip']) * scale_factor_body * np.array([-1,1,-1])
+    new_body_dict['right_knee'] = (body_dict['right_hip'] - body_dict['right_knee']) * np.array([-1,1,-1])
+    new_body_dict['right_knee'] = new_body_dict['right_knee'] * unity_up_leg_length / np.linalg.norm(new_body_dict['right_knee'])
+    new_body_dict['right_ankle'] = (body_dict['right_knee'] - body_dict['right_ankle']) * np.array([-1,1,-1])
+    new_body_dict['right_ankle'] = new_body_dict['right_ankle'] * unity_lower_leg_length / np.linalg.norm(new_body_dict['right_ankle'])
+
+    new_body_dict['left_hip'] = (body_dict['ass'] - body_dict['left_hip']) * scale_factor_body * np.array([-1,1,-1])
+    new_body_dict['left_knee'] = (body_dict['left_hip'] - body_dict['left_knee']) * np.array([-1,1,-1])
+    new_body_dict['left_knee'] = new_body_dict['left_knee'] * unity_up_leg_length / np.linalg.norm(new_body_dict['left_knee'])
+    new_body_dict['left_ankle'] = (body_dict['left_knee'] - body_dict['left_ankle']) * np.array([-1,1,-1])
+    new_body_dict['left_ankle'] = new_body_dict['left_ankle'] * unity_lower_leg_length / np.linalg.norm(new_body_dict['left_ankle'])
     
-    new_body_dict['left_shoulder'] = (body_dict['chest'] - body_dict['left_shoulder']) * scale_factor * np.array([-1,1,-1])
-    new_body_dict['left_elbow'] = (body_dict['left_shoulder'] - body_dict['left_elbow']) * scale_factor * np.array([-1,1,-1])
-    new_body_dict['left_wrist'] = (body_dict['left_elbow'] - body_dict['left_wrist']) * scale_factor * np.array([-1,1,-1])
+    new_body_dict['right_shoulder'] = (body_dict['chest'] - body_dict['right_shoulder']) * scale_factor_body * np.array([-1,1,-1])
+    new_body_dict['right_elbow'] = (body_dict['right_shoulder'] - body_dict['right_elbow']) * np.array([-1,1,-1])
+    new_body_dict['right_elbow'] = new_body_dict['right_elbow'] * unity_arm_length / np.linalg.norm(new_body_dict['right_elbow'])
+    new_body_dict['right_wrist'] = (body_dict['right_elbow'] - body_dict['right_wrist']) * np.array([-1,1,-1])
+    new_body_dict['right_wrist'] = new_body_dict['right_wrist'] * unity_forearm_length / np.linalg.norm(new_body_dict['right_wrist'])
+    
+    new_body_dict['left_shoulder'] = (body_dict['chest'] - body_dict['left_shoulder']) * scale_factor_body * np.array([-1,1,-1])
+    new_body_dict['left_elbow'] = (body_dict['left_shoulder'] - body_dict['left_elbow']) * np.array([-1,1,-1])
+    new_body_dict['left_elbow'] = new_body_dict['left_elbow'] * unity_arm_length / np.linalg.norm(new_body_dict['left_elbow'])
+    new_body_dict['left_wrist'] = (body_dict['left_elbow'] - body_dict['left_wrist']) * np.array([-1,1,-1])
+    new_body_dict['left_wrist'] = new_body_dict['left_wrist'] * unity_forearm_length / np.linalg.norm(new_body_dict['left_wrist'])
     
     return new_body_dict
+
+
+
+def knee_check(coords:dict)->dict:
+    """
+    Corrects knee rotation. Z axis goes into the screen
+    """
+    scale_knee = 1
+    flag_knee = False
+    # TODO: create flag for theses conditions to check if it works properly
+    if (coords['left_knee'][2] > coords['left_ankle'][2]): #and (coords['left_knee'][2] > coords['left_hip'][2]):
+        new_depth = coords['left_ankle'][2] - (coords['left_knee'][2] - coords['left_ankle'][2])
+        coords['left_knee'][2] = new_depth / scale_knee
+        flag_knee = True
+        print("Incorrect left knee rotation")
+    
+    if (coords['right_knee'][2] > coords['right_ankle'][2]): #and (coords['right_knee'][2] > coords['right_hip'][2]):
+        new_depth = coords['right_ankle'][2] - (coords['right_knee'][2] - coords['right_ankle'][2])
+        coords['right_knee'][2] = new_depth / scale_knee
+        flag_knee = True
+        print("Incorrect rigth knee rotation")
+
+    if flag_knee:
+        print("Incorrect knee rotation")
+
+    return coords
+
+
+def knee_check2(coords:dict)->dict:
+    """
+    Corrects knee rotation. Z axis goes into the screen.
+    Version 2 of the knee check algorithm. It is based on the angles between bones
+    """
+
+    flag_knee = False
+
+    left_knee_angle = point_angle(coords['left_hip'][[1,2]], coords['left_ankle'][[1,2]])
+    right_knee_angle = point_angle(coords['right_hip'][[1,2]], coords['right_ankle'][[1,2]])
+
+    if left_knee_angle > 0:
+        coords['left_knee'][2] = coords['left_ankle'][2]
+        flag_knee = True
+
+    if right_knee_angle > 0:
+        coords['right_knee'][2] = coords['right_ankle'][2]
+        flag_knee = True
+
+    if flag_knee:
+        print("Incorrect knee rotation")
+
+    return coords
+
+
+
+def get_ass_location(image_body_length:float, unity_body_length:float, unity_ass_position:float)->np.array:
+    z_coord = unity_body_length / (unity_ass_position * image_body_length) - unity_ass_position
+    new_z_coord = - z_coord / 2
+    # new_z_coord = unity_ass_position + z_coord
+    
+    return np.array([0, 1.2, new_z_coord], dtype=np.float32)